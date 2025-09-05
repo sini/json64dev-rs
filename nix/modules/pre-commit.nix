9163abef6f9d10c2828b1e11c9e11486f2c21210
Jason Bowman <jason@json64.dev>
2025-09-05 12:44:19 -0700

    initial commit

---
+++ nix/modules/pre-commit.nix
@@ -0,0 +1,22 @@
+{ inputs, ... }:
+{
+  imports = [
+    (inputs.git-hooks + /flake-module.nix)
+  ];
+  perSystem =
+    {
+      config,
+      self',
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      pre-commit.settings = {
+        hooks = {
+          nixpkgs-fmt.enable = true;
+          rustfmt.enable = true;
+        };
+      };
+    };
+}
